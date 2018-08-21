9a9521ddfada246a813667a6fe42bc80464cdbde
Kevin W <kjoewill@gmail.com>
2018-08-21 15:33:23 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 
-speakers = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   "Hello, my name is #{name}."
